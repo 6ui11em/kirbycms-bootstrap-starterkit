299691a6b864c88e00289283984eafa0f3b2bfb7
6ui11em <6ui11em1@gmail.com>
2015-07-08 23:24:16 +0200

    Fix bug class active on menu
    Signed-off-by: 6ui11em <6ui11em1@gmail.com>

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -2,19 +2,19 @@
 	<ul class="nav navbar-nav">
     <?php foreach($pages->visible() as $p): ?>
       <?php if($p->hasVisibleChildren()): ?>
-				<li class="dropdown">
-					<a class="dropdown-toggle<?php e($p->isOpen(), ' active') ?>" data-toggle="dropdown" role="button" aria-expanded="false" href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?> <span class="caret"></span></a>
+				<li class="dropdown<?php e($p->isOpen(), ' active') ?>">
+					<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?> <span class="caret"></span></a>
 					<ul class="dropdown-menu" role="menu">
 						<?php foreach($p->children()->visible() as $p): ?>
-						<li>
-							<a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
+						<li<?php e($p->isOpen(), ' class="active"') ?>>
+							<a href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
 						</li>
 						<?php endforeach ?>
 					</ul>
 				</li>
       <?php else : ?>
-				<li>
-					<a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
+				<li<?php e($p->isOpen(), ' class="active"') ?>>
+					<a href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
 				</li>
       <?php endif; ?>
 
